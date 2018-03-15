b6bd896c68ff24391ffabaa3b7c73322ef2293be
Federal0888 <jeremee96@gmail.com>
2018-03-15 20:59:01 +0300

    ddfdfdfdfd

--- src/index.js
+++ src/index.js
@@ -1,27 +1,97 @@
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
+    constructor(initialValue) {
+        this.initialValue = initialValue;
+    }
 
-  add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
-  }
+    add(number) {
+        this.initialValue += ` + ${number}`;
+        return this;
+    }
 
-  multiply(number) {
-    // your implementation
-  }
+    subtract(number) {
+        this.initialValue += ` - ${number}`;
+        return this;
+    }
 
-  devide(number) {
-    // your implementation
-  }
+    multiply(number) {
+        this.initialValue += ` * ${number}`;
+        return this;
+    }
 
-  pow(number) {
-    // your implementation
-  }
+    devide(number) {
+        this.initialValue += ` / ${number}`;
+        return this;
+    }
+
+    pow(number) {
+        this.initialValue += ` ** ${number}`;
+        return this;
+    }
+
+    changer() {
+        let stroka = this.initialValue;
+        let mass = stroka.split(' ');
+        let otv = 0;
+        let i = mass.length - 1;
+
+        while (i > -1) {
+            if (mass[i] == '**') {
+                otv = mass[i - 1] ** mass[i + 1];
+                otv = otv + '';
+                mass.splice(i - 1, 3, otv);
+                i++;
+            }
+            i--;
+        }
+        i = 0;
+        while (i < mass.length) {
+            if (mass[i] == '*') {
+                otv = mass[i - 1] * mass[i + 1];
+                otv = otv + '';
+                mass.splice(i - 1, 3, otv);
+                i--;
+            }
+            i++;
+        }
+        i = 0;
+        while (i < mass.length) {
+            if (mass[i] == '/') {
+                otv = mass[i - 1] / mass[i + 1];
+                otv = otv + '';
+                mass.splice(i - 1, 3, otv);
+                i--;
+            }
+            i++;
+        }
+        i = 0;
+        while (i < mass.length) {
+            if (mass[i] == '-') {
+                otv = mass[i - 1] - mass[i + 1];
+                otv = otv + '';
+                mass.splice(i - 1, 3, otv);
+                i--;
+            }
+            i++;
+        }
+        i = 0;
+        while (i < mass.length) {
+            if (mass[i] == '+') {
+                otv = +mass[i - 1] + +mass[i + 1];
+                otv = otv + '';
+                mass.splice(i - 1, 3, otv);
+                i--;
+            }
+            i++;
+        }
+        this.initialValue = +mass[0];
+    }
+
+    toString() {
+        this.changer();
+        return this.initialValue;
+    }
+
+
 }
 
 module.exports = SmartCalculator;
